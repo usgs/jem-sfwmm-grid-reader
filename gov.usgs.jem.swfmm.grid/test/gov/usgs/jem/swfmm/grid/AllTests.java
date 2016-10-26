98d68ba9fb8a9c57fdd786cda4cd8cc22c196ac5
Mark McKelvy <mckelvym@usgs.gov>
2016-10-26 09:24:58 -0400

    WARC-218 Added AllTests unit testing suite.

---
+++ gov.usgs.jem.swfmm.grid/test/gov/usgs/jem/swfmm/grid/AllTests.java
@@ -0,0 +1,71 @@
+package gov.usgs.jem.swfmm.grid;
+
+import java.io.File;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import gov.usgs.jem.swfmm.grid.input.SeekableDataFileInputStreamImplTest;
+
+/**
+ * Tests all cases
+ *
+ * @author mckelvym
+ * @since Oct 26, 2016
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ SeekableDataFileInputStreamImplTest.class,
+		GIOHeaderTest.class })
+public class AllTests
+{
+	/**
+	 * Ensure that a testing class has defined all required testing methods.
+	 *
+	 * @param p_ClassToTest
+	 *            the class that is being tested
+	 * @param p_TestingClass
+	 *            the class that is doing the testing
+	 * @since Oct 26, 2016
+	 */
+	public static void assertHasRequiredMethods(final Class<?> p_ClassToTest,
+			final Class<?> p_TestingClass)
+	{
+		try
+		{
+			setUpBeforeClass();
+		}
+		catch (final Exception e)
+		{
+			e.printStackTrace();
+			Assert.fail("Unable to set up tests: " + e.getMessage());
+		}
+		try
+		{
+			Tests.assertHasRequiredMethods(p_ClassToTest, p_TestingClass);
+		}
+		catch (final NoSuchMethodException e)
+		{
+			Assert.fail(e.getMessage());
+		}
+	}
+
+	/**
+	 * @return the test file to use
+	 * @since Oct 26, 2016
+	 */
+	public static final File getTestFile()
+	{
+		return new File("test/data/eomth_stage.bin");
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+		BasicConfigurator.configure();
+	}
+}
