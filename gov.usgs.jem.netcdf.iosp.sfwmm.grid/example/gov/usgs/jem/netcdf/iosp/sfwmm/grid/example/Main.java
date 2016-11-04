26895afabce9223872b7e672247d5d907de44655
Mark McKelvy <mckelvym@usgs.gov>
2016-11-04 12:04:31 -0400

    WARC-218 Added Main in gov.usgs.jem.netcdf.iosp.sfwmm.grid/example howing how to convert from the 2x2’s .bin format to NetCDF

---
+++ gov.usgs.jem.netcdf.iosp.sfwmm.grid/example/gov/usgs/jem/netcdf/iosp/sfwmm/grid/example/Main.java
@@ -0,0 +1,65 @@
+package gov.usgs.jem.netcdf.iosp.sfwmm.grid.example;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+
+import gov.usgs.jem.netcdf.iosp.sfwmm.grid.AllTests;
+import gov.usgs.jem.netcdf.iosp.sfwmm.grid.SFWMMGridIOSP;
+import ucar.nc2.FileWriter2;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.NetcdfFileWriter.Version;
+
+/**
+ * Example showing how to convert from the 2x2’s .bin format to a NetCDF file
+ *
+ * @author mckelvym
+ * @since Nov 4, 2016
+ *
+ */
+public final class Main
+{
+	/**
+	 * Class logger
+	 */
+	private static final org.apache.log4j.Logger log = org.apache.log4j.Logger
+			.getLogger(Main.class);
+
+	public static void main(final String[] args)
+			throws IOException, IllegalAccessException, InstantiationException
+	{
+		BasicConfigurator.configure();
+
+		final String inputFilePath = AllTests.getTestFile().getAbsolutePath();
+		final String outputFilePath = inputFilePath + ".nc";
+
+		/**
+		 * Register the service provider with the NetCDF library
+		 */
+		NetcdfFile.registerIOProvider(SFWMMGridIOSP.class);
+
+		/**
+		 * Proceed to open file through CDM as any other NetCDF file
+		 */
+		try (NetcdfFile nc = NetcdfFile.open(inputFilePath))
+		{
+			/**
+			 * Copy to 'real' NetCDF
+			 */
+			final FileWriter2 writer = new FileWriter2(nc, outputFilePath,
+					Version.netcdf3, null);
+			writer.write();
+		}
+
+		/**
+		 * Log CDL
+		 */
+		try (NetcdfFile nc = NetcdfFile.open(outputFilePath))
+		{
+			final ByteArrayOutputStream out = new ByteArrayOutputStream();
+			nc.writeCDL(out, false);
+			log.info(out);
+		}
+	}
+}
---
+++ gov.usgs.jem.netcdf.iosp.sfwmm.grid/example/gov/usgs/jem/netcdf/iosp/sfwmm/grid/example/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains example program(s) showing how to convert from the 2x2’s .bin format
+ * to a NetCDF file
+ *
+ * @author mckelvym
+ * @since Nov 4, 2016
+ *
+ */
+package gov.usgs.jem.netcdf.iosp.sfwmm.grid.example;
